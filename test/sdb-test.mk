85b473925c1b95145a41d36ae429174231f23b14
pancake <pancake@nopcode.org>
2026-03-10 10:55:20 +0100

    Linking fixes for the testsuite

--- test/sdb-test.mk
+++ test/sdb-test.mk
@@ -4,6 +4,13 @@ CURRENT_DIR:=$(abspath $(patsubst %/,%,$(dir $(MAKEFILE_PATH))))
 SRCDIR=${CURRENT_DIR}/../src
 INCDIR=${CURRENT_DIR}/../include
 BASEDIR?=${SRCDIR}
+SDB_LIB?=${BASEDIR}/libsdb.a
 SDB_CFLAGS+=-I${INCDIR} -I${BASEDIR} ${USER_CFLAGS}
-SDB_LDFLAGS+=${BASEDIR}/libsdb.a ${USER_LDFLAGS}
+SDB_LDFLAGS+=${SDB_LIB} ${USER_LDFLAGS}
 SDB=${BASEDIR}/sdb
+
+${SDB_LIB}:
+	$(MAKE) -C ${BASEDIR} libsdb.a
+
+${SDB}:
+	$(MAKE) -C ${BASEDIR} sdb
